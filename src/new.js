89c20976fb70b7af43b10cd6fbdf6d901d2eee3c
Amal Asok <amal@applab.qa>
2022-04-18 09:18:01 +0530

    added new

--- src/new.js
+++ src/new.js
@@ -1,27 +1,39 @@
-<View style={{ flexDirection: 'row', height: 60, margin: 16 }}>
-                <Image style={{ height: 70, width: 90, top: -12, right: 14 }}
-                    source={item.image} />
-                <TouchableOpacity onPress={() => props.navigation.navigate('Workers')}>
-                    <Text style={{ fontSize: 16, fontWeight: 'bold', padding: 10, left: 8, fontFamily: 'MontserratAlternates-Medium'}}>{item.title}</Text>
-                </TouchableOpacity>
+import React,{Component} from 'react'
+import { View, StyleSheet, Text, FlatList} from 'react-native'
+export default class new extends Component{
 
+    constructor (){
+        super()
+        this.state = {
+            DATA : []
+        }
+    }
+    getData (){
+        fetch ('')
+        .then( (response) => response.json())
+        .then ( (response) => {
+            this.setState({ DATA :  response})
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+    componentDidMount (){
+        this.getData()
+    }
+    render(){
+const renderItem = ( {item}) => (
+    <View>
+        <Text>{item.title}</Text>
+    </View>
+)
+        return(
+            <View>
+                <FlatList 
+                data = {this.state.DATA}
+                renderItem = {renderItem}/>
             </View>
-
-
-
-<FlatList
-keyExtractor={(item) => item.id}
-data={DATA} horizontal={true}
-renderItem={({ item, index }) => {
-    return (
-        <View style={{ marginTop: 20 }}>
-            <View style={{ height: height * 0.006, width: width * 0.093, backgroundColor: '#3D989F', marginHorizontal: 10 }}>
-            </View>
-        </View>
-    )
-}} />
-
-
-<TouchableOpacity onPress={() => pageScroll()}>
-<Text style={{ fontSize: 16, color: '#AFABC4', }}>Skip</Text>
-</TouchableOpacity>
+        )
+    }
+}
